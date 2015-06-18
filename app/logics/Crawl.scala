02908419a9d8b4b289a58ab4a714df49ae967ee9
haya14busa <hayabusa1419@gmail.com>
2015-06-19 04:44:25 +0900

    Do not re-calculate related docs if there are no need

--- app/logics/Crawl.scala
+++ app/logics/Crawl.scala
@@ -23,37 +23,41 @@ object Crawl {
           .withFilter(a => !(ids contains a.guid)) // filter existing articles
           .par.flatMap(toArticle)
       play.Logger.info(s"[Crawler] end scraping articles ${articles.length}")
-
-      Future {
-        play.Logger.info("[Crawler] start tfidf calculation")
-        val dvs: List[DocumentVector] = DocVector.tfidf(
-          articles.map(a => RawDocument(guid = a.guid, body = a.content)).toList
-            :::
-            existingArticles.map(ea => RawDocument(guid = ea._1, body = ea._2)).toList
-        )
-        dvs.par.foreach { dv =>
-          val relatedArticles = DocVector.findSimilarDocs(dv, dvs, n = 5)
-          play.Logger.info(s"[Similer #${dv.guid}] ${relatedArticles map (_.guid)}")
-          val as = ((relatedArticles.map(r => Some(r.guid)) ::: List.fill[Option[Long]](5)(None)) take 5)
-          dao.RelatedocsDAO.upsert(
-            RelatedocsRow(
-              guid = dv.guid,
-              rank1 = as(0),
-              rank2 = as(1),
-              rank3 = as(2),
-              rank4 = as(3),
-              rank5 = as(4)
-            )
-          )
-        }
-        play.Logger.info("[Crawler] end tfidf calculation")
-      }
-
-      Future {
+      if (articles.isEmpty)
+        play.Logger.info(s"[Crawler] Do not re-calculate related doc because there are no new documents")
+      else {
+        calcRelatedArticles(articles, existingArticles)
         play.Logger.info("[Crawler] start to insert articles")
         articles foreach dao.ArticleDAO.insert
         play.Logger.info("[Crawler] end inserting articles")
       }
+    }
+  }
+
+  private def calcRelatedArticles(articles: ParSeq[Article], existingArticles: Set[(Long, String)]) = {
+    Future {
+      play.Logger.info("[Crawler] start tfidf calculation")
+      val dvs: List[DocumentVector] = DocVector.tfidf(
+        articles.map(a => RawDocument(guid = a.guid, body = a.content)).toList
+          :::
+          existingArticles.map(ea => RawDocument(guid = ea._1, body = ea._2)).toList
+      )
+      dvs.par.foreach { dv =>
+        val relatedArticles = DocVector.findSimilarDocs(dv, dvs, n = 5)
+        play.Logger.info(s"[Similer #${dv.guid}] ${relatedArticles map (_.guid)}")
+        val as = ((relatedArticles.map(r => Some(r.guid)) ::: List.fill[Option[Long]](5)(None)) take 5)
+        dao.RelatedocsDAO.upsert(
+          RelatedocsRow(
+            guid = dv.guid,
+            rank1 = as(0),
+            rank2 = as(1),
+            rank3 = as(2),
+            rank4 = as(3),
+            rank5 = as(4)
+          )
+        )
+      }
+      play.Logger.info("[Crawler] end tfidf calculation")
     }
   }
 
