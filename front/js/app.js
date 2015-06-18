3fdce154e5ac5fdb6a57ace04e10a58cd7c9323c
haya14busa <hayabusa1419@gmail.com>
2015-06-19 07:55:16 +0900

    Add link to title

--- front/js/app.js
+++ front/js/app.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import { RouteHandler } from 'react-router';
+import { Link, RouteHandler } from 'react-router';
 import CategoryList from './components/CategoryList.js'
 
 export default class App {
   render() {
     return (
       <div>
-        <CategoryList cgid="top"/>
+        <h1 id='title'><Link to='rss'>livedoor news reader</Link></h1>
+        <CategoryList cgid='top'/>
         <hr />
         <RouteHandler {...this.props} />
       </div>
